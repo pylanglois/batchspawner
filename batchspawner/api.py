5a5de660214de4924b81cca8e115e5a0540be992
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-03-28 15:18:18 -0400

    Identify spawner in api with auth token

--- batchspawner/api.py
+++ batchspawner/api.py
@@ -5,12 +5,18 @@ from jupyterhub.apihandlers import  APIHandler, default_handlers
 class BatchSpawnerAPIHandler(APIHandler):
     @web.authenticated
     def post(self):
-        """POST set user's spawner port number"""
+        """POST set user spawner data"""
+        token = self.get_auth_token()
         user = self.current_user
         data = self.get_json_body()
+        spawner = user.spawner
+        for x in user.spawners.values():
+            if x.api_token == token:
+                spawner = x
+                break
         for key, value in data.items():
-            if hasattr(user.spawner, key):
-                setattr(user.spawner, key, value)
+            if hasattr(spawner, key):
+                setattr(spawner, key, value)
         self.finish(json.dumps({"message": "BatchSpawner data configured"}))
         self.set_status(201)
 
